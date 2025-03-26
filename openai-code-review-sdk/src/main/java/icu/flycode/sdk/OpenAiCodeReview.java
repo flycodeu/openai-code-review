aa52169d45ac1cfa50aebb1d7a7d3ad6bbd7a921
flycodeu <1785613048@qq.com>
2025-03-26 10:21:20 +0800

    feat: 添加提交代码记录读取

--- openai-code-review-sdk/src/main/java/icu/flycode/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/icu/flycode/sdk/OpenAiCodeReview.java
@@ -1,7 +1,31 @@
 package icu.flycode.sdk;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class OpenAiCodeReview {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("测试执行");
+        // 代码评审
+        // 1. 读取Git Diff更改记录
+        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD~1", "HEAD");
+        processBuilder.directory(new File("."));
+        Process process = processBuilder.start();
+
+        // 读取输出流
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        StringBuilder diffStr = new StringBuilder();
+        while ((line = bufferedReader.readLine()) != null) {
+            diffStr.append(line);
+        }
+
+        // 2. 获取退出码
+        int exitCode = process.waitFor();
+        System.out.println("Exited with code:" + exitCode);
+        // 3. 返回读取数据
+        System.out.println("评审代码" + diffStr.toString());
     }
 }
