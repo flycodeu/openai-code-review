d548ab953e7f9140cd6fe70049c7682416a90902
flycodeu <1785613048@qq.com>
2025-03-26 18:19:05 +0800

    feat: 代码评审v1

--- openai-code-review-sdk/src/main/java/icu/flycode/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/icu/flycode/sdk/OpenAiCodeReview.java
@@ -48,24 +48,13 @@ public class OpenAiCodeReview {
 
         HttpsURLConnection httpsURLConnection = getHttpsURLConnection(token);
 
-        String jsonInputString = "{"
-                + "\"model\":\"glm-4-flash\","
-                + "\"messages\": ["
-                + "    {"
-                + "        \"role\": \"user\","
-                + "        \"content\": \"你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码为: " + diffCode + "\""
-                + "    }"
-                + "]"
-                + "}";
-
-
 
         ChatCompletionRequest chatCompletionRequest = new ChatCompletionRequest();
         chatCompletionRequest.setModel(Model.GLM_4_FLASH.getCode());
         chatCompletionRequest.setMessages(new ArrayList<ChatCompletionRequest.Prompt>(){
             {
                 add(new ChatCompletionRequest.Prompt("user","你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码为: "));
-                add(new ChatCompletionRequest.Prompt("content",diffCode));
+                add(new ChatCompletionRequest.Prompt("user",diffCode));
             }
         });
 
