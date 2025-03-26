6ff9b2204eb998bafd310922481e22904e2a455f
flycodeu <1785613048@qq.com>
2025-03-26 16:32:13 +0800

    feat: 代码评审

--- openai-code-review-sdk/src/main/java/icu/flycode/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/icu/flycode/sdk/OpenAiCodeReview.java
@@ -1,13 +1,16 @@
 package icu.flycode.sdk;
 
 import com.alibaba.fastjson2.JSON;
+import icu.flycode.sdk.domain.models.ChatCompletionRequest;
 import icu.flycode.sdk.domain.models.ChatCompletionSyncResponse;
+import icu.flycode.sdk.domain.models.Model;
 import icu.flycode.sdk.utils.BearerTokenUtils;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.io.*;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 public class OpenAiCodeReview {
     public static void main(String[] args) throws Exception {
@@ -62,8 +65,18 @@ public class OpenAiCodeReview {
                 + "}";
 
 
+
+        ChatCompletionRequest chatCompletionRequest = new ChatCompletionRequest();
+        chatCompletionRequest.setModel(Model.GLM_4_FLASH.getCode());
+        chatCompletionRequest.setMessages(new ArrayList<ChatCompletionRequest.Prompt>(){
+            {
+                add(new ChatCompletionRequest.Prompt("user","你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码为: "));
+                add(new ChatCompletionRequest.Prompt("content",diffCode));
+            }
+        });
+
         try (OutputStream os = httpsURLConnection.getOutputStream()) {
-            byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
+            byte[] input = JSON.toJSONString(chatCompletionRequest).getBytes(StandardCharsets.UTF_8);
             os.write(input, 0, input.length);
         }
 
