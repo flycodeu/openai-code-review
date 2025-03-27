bdc97858ad03b090c9d263e7a2e8f3a43dc4d08b
flycodeu <1785613048@qq.com>
2025-03-27 10:50:37 +0800

    feat: 添加调用公众号

--- openai-code-review-sdk/src/main/java/icu/flycode/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/icu/flycode/sdk/OpenAiCodeReview.java
@@ -184,6 +184,7 @@ public class OpenAiCodeReview {
         Message message = new Message();
         message.put("project", "openai-code-review-logs");
         message.put("review", logUrl);
+        message.setUrl(logUrl);
         String url = String.format("https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=%s", accessToken);
         sendPostRequest(url, JSON.toJSONString(message));
     }
--- openai-code-review-sdk/src/main/java/icu/flycode/sdk/domain/models/Message.java
+++ openai-code-review-sdk/src/main/java/icu/flycode/sdk/domain/models/Message.java
@@ -7,7 +7,7 @@ public class Message {
     // 发送给指定用户
     private String touser = "oNbqZ6vShgDLdznFaExcZl0yJCz8";
     // 模板id
-    private String template_id = "TfCvx0efPjZz6-nZDZjcUzwGplSw7R60RC_z1pPpREU";
+    private String template_id = "N5_87CxM1EQUlC__3DR8tovPGRfD3_Wz4sCQgPPwM40";
     // 用户点击可以跳转指定链接
     private String url = "https://github.com/flycodeu/openai-code-review-logs.git";
     private Map<String, Map<String, String>> data = new HashMap<>();
